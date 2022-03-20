e10ce5f849a600409bffbdfc9fd28e28818234a5
leib <leib1368@gmail.com>
2022-03-20 13:53:13 +0800

    该换注册中心nocas

--- springcloud-provider-dept-8003/src/main/java/leib/demo/DeptProvider_8003.java
+++ springcloud-provider-dept-8003/src/main/java/leib/demo/DeptProvider_8003.java
@@ -1,16 +1,16 @@
 package leib.demo;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author:Leib
  * @Date 2020/5/9 13:20
  */
 @SpringBootApplication
-@EnableEurekaClient //服务启动后自动注册到Eureka
 @EnableDiscoveryClient  //服务发现
 public class DeptProvider_8003 {
     public static void main(String[] args) {
