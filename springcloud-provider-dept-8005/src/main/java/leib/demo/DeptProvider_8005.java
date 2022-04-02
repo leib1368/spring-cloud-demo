79f602411672af47cc1355baa9e34694cd64fbef
余力博 <lib.yu@sunyard.com>
2022-04-02 10:33:30 +0800

    swagger

---
+++ springcloud-provider-dept-8005/src/main/java/leib/demo/DeptProvider_8005.java
@@ -0,0 +1,13 @@
+package leib.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@SpringBootApplication
+@EnableOpenApi
+public class DeptProvider_8005 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8005.class, args);
+    }
+}
